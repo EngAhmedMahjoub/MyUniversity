e319223877ffbe69db1c078b6f7780f573e95a8e
EngAhmedMahjoub <EngAhmedMahjoub@gmail.com>
2024-05-05 14:18:21 +1000

    add map and about us pages

---
+++ src/pages/AboutUsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutUsPage() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Information about the university.</p>
+        </div>
+    );
+}
+
+export default AboutUsPage;
