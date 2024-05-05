9b1bde29de1458ef431536e1f16dac3eca19221c
EngAhmedMahjoub <EngAhmedMahjoub@gmail.com>
2024-05-05 12:34:34 +1000

    add home page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to My University</h1>
+            <p>Find your way around with our interactive campus map and stay updated with the latest news.</p>
+            {/* You can add more components here like NewsFeed or CampusMap */}
+        </div>
+    );
+}
+
+export default HomePage;
