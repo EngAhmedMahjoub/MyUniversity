b724f7325edaa625132ccac531d9f7fb65f76c7a
Ahmed Ali Mahjoub <110306125+EngAhmedMahjoub@users.noreply.github.com>
2024-05-08 22:55:30 +0000

    style map  page

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,11 +1,18 @@
+// src/pages/Map.js
 import React from 'react';
+import Layout from '../components/Layout';
 
-function Map() {
-    return (
-        <div>
-            <h1>الخريطة</h1>
-        </div>
-    );
-}
+const Map = () => {
+  return (
+    <Layout>
+      <h1 style={{ textAlign: 'center' }}>خريطة الحرم الجامعي</h1>
+      <p>استخدم الخريطة أدناه للتنقل في الحرم الجامعي.</p>
+      {/* Placeholder for map integration */}
+      <div style={{ height: '500px', backgroundColor: '#eee', textAlign: 'center', lineHeight: '500px' }}>
+        [خريطة تفاعلية هنا]
+      </div>
+    </Layout>
+  );
+};
 
 export default Map;
