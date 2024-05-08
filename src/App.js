8aaa83d854c9e8befd2b5a7733f87d3a8d41574a
Ahmed Ali Mahjoub <110306125+EngAhmedMahjoub@users.noreply.github.com>
2024-05-08 22:57:56 +0000

    remove navbar duplication

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Map from './pages/Map';
 import AboutUs from './pages/AboutUs';
-import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Router>
-        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/map" element={<Map />} />
