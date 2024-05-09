5bd2ffaf89ce68de6a00c1a9db5cd99d65133d0e
Ahmed Ali Mahjoub <110306125+EngAhmedMahjoub@users.noreply.github.com>
2024-05-09 06:07:18 +0000

    add map view component to map page

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,4 +1,3 @@
-// src/pages/Map.js
 import React from 'react';
 import Layout from '../components/Layout';
 
@@ -7,10 +6,11 @@ const Map = () => {
     <Layout>
       <h1 style={{ textAlign: 'center' }}>خريطة الحرم الجامعي</h1>
       <p>استخدم الخريطة أدناه للتنقل في الحرم الجامعي.</p>
-      {/* Placeholder for map integration */}
-      <div style={{ height: '500px', backgroundColor: '#eee', textAlign: 'center', lineHeight: '500px' }}>
-        [خريطة تفاعلية هنا]
-      </div>
+      <iframe
+        src="https://app.mappedin.com/map/66368db3269972f02bf839c6/directions?floor=m_7fbd89a31da73e80&location=s_0a8a17ac9259439a&departure=24.02651374489289%2C38.201534435989466%2Cm_7fbd89a31da73e80"
+        style={{ width: '100%', height: '500px', border: 'none' }}
+        allowFullScreen
+      />
     </Layout>
   );
 };
