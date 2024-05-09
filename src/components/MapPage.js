0b0eb4b4e4dfe6701b492570de1e5e74d0a5b014
Ahmed Ali Mahjoub <110306125+EngAhmedMahjoub@users.noreply.github.com>
2024-05-09 15:28:41 +0000

    fix map layout

--- src/components/MapPage.js
+++ src/components/MapPage.js
@@ -3,11 +3,11 @@ import Layout from '../components/Layout';
 
 const MapPage = () => {
   return (
-    <Layout>
-      <h1 style={{ textAlign: 'center' }}>خريطة الحرم الجامعي</h1>
-      <p>استخدم الخريطة أدناه للتنقل في الحرم الجامعي.</p>
-      <MapPage></MapPage>
-    </Layout>
+      <iframe
+        src="https://app.mappedin.com/map/66368db3269972f02bf839c6/directions?floor=m_7fbd89a31da73e80&location=s_0a8a17ac9259439a&departure=24.02651374489289%2C38.201534435989466%2Cm_7fbd89a31da73e80"
+        style={{ width: '100%', height: '500px', border: 'none' }}
+        allowFullScreen
+      />
   );
 };
 
--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import MapPage from '../components/MapPage';
 
 const Map = () => {
   return (
     <Layout>
       <h1 style={{ textAlign: 'center' }}>خريطة الحرم الجامعي</h1>
       <p>استخدم الخريطة أدناه للتنقل في الحرم الجامعي.</p>
-      <iframe
-        src="https://app.mappedin.com/map/66368db3269972f02bf839c6/directions?floor=m_7fbd89a31da73e80&location=s_0a8a17ac9259439a&departure=24.02651374489289%2C38.201534435989466%2Cm_7fbd89a31da73e80"
-        style={{ width: '100%', height: '500px', border: 'none' }}
-        allowFullScreen
-      />
+      <MapPage></MapPage>
     </Layout>
   );
 };
