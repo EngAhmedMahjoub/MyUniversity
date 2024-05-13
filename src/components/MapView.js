885ebc0e070a202f90a994b8b1c4d73d70a3ddd3
Ahmed Ali Mahjoub <110306125+EngAhmedMahjoub@users.noreply.github.com>
2024-05-13 08:08:04 +0000

    update map link

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MapView = () => {
   return (
       <iframe
-        src="https://app.mappedin.com/map/66368db3269972f02bf839c6/directions?floor=m_7fbd89a31da73e80&location=s_0a8a17ac9259439a&departure=24.02651374489289%2C38.201534435989466%2Cm_7fbd89a31da73e80"
+        src="https://app.mappedin.com/map/66368db3269972f02bf839c6?floor=m_7fbd89a31da73e80"
         style={{ width: '100%', height: '500px', border: 'none' }}
         allowFullScreen
       />
